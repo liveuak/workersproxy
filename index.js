00155853b57afc3340321f792af3071db3c7aebf
Siujoeng Lau <admin@siujoeng-lau.com>
2019-09-11 20:34:19 +0800

    Add IP address filter and webpage redirector according to user's device type.
    Signed-off-by: Siujoeng Lau <admin@siujoeng-lau.com>

--- index.js
+++ index.js
@@ -1,31 +1,62 @@
 const main_domain = 'https://cdn.reverse-proxy.live/'
+
 const workers_domain = 'https://google.xasiimov.workers.dev'
+
 const upstream = 'https://www.google.com/'
+
+const upstream_mobile = 'https://www.google.com/'
+
 const blocked_region = ['CN', 'KP', 'SY', 'PK', 'CU']
 
+const blocked_ip_address = ['0.0.0.0', '10.0.0.0']
+
 addEventListener('fetch', event => {
-    event.respondWith(fetchAndApply(event.request))
+    event.respondWith(fetchAndApply(event.request));
 })
 
 async function fetchAndApply(request) {
 
-    const region = request.headers.get('Cf-Ipcountry').toUpperCase()
-    let url = request.url
-    if (blocked_region.includes(region)) {
-        let response = new Response('Access denied: WorkersProxy is not available in your region yet.', {
-            status: 403
-        })
-        return response
-    } else {
-        url = url.replace(main_domain, upstream)
-        url = url.replace(workers_domain, upstream)
-        let method = request.method
-        let headers = request.headers
+    const region = request.headers.get('cf-ipcountry').toUpperCase();
+    const ip_address = request.headers.get('cf-connecting-ip');
+    const user_agent = request.headers.get('user-agent');
+    let response = null;
+    let url = request.url;
 
-        let response = fetch(url, {
+    if (device_status(user_agent)){
+        url = url.replace(main_domain, upstream);
+        url = url.replace(workers_domain, upstream);
+    } else {
+        url = url.replace(main_domain, upstream_mobile);
+        url = url.replace(workers_domain, upstream_mobile);
+    }
+
+    if (blocked_region.includes(region)) {
+        response = new Response('Access denied: WorkersProxy is not available in your region yet.', {
+            status: 403
+        });
+    } else if(blocked_ip_address.includes(ip_address)){
+        response = new Response('Access denied: Your IP address is blocked by WorkersProxy.', {
+            status: 403
+        });
+    } else{
+        let method = request.method;
+        let headers = request.headers;
+        response = fetch(url, {
             method: method,
             headers: headers
         })
-        return response
     }
+    return response;
+}
+
+function device_status (userAgentInfo) {
+    var Agents = ["Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod"];
+    var flag = true;
+    for (var v = 0; v < Agents.length; v++) {
+        if (userAgentInfo.indexOf(Agents[v]) > 0) {
+            flag = false;
+            break;
+        }
+    }
+    return flag;
 }
